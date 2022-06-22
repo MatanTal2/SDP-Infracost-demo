9605e58d2cc7945e66d2de8bf50ae0ef32589ff4
Matan Tal <matantal2@gmail.com>
2022-06-22 14:18:37 +0300

    type

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "m5.4xlarge"
+  instance_type = "t3.large"
 
   root_block_device {
     volume_size = 50
